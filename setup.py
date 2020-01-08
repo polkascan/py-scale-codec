b4b039204658891d182910d5c4b4f20bd4869179
Arjan Zijderveld <arjan@web3scan.eu>
2020-01-08 11:41:02 +0100

    Include type_registry json files in package

--- setup.py
+++ setup.py
@@ -165,16 +165,16 @@ setup(
     # If using Python 2.6 or earlier, then these have to be included in
     # MANIFEST.in as well.
 
-    # package_data={  # Optional
-    #     'sample': ['package_data.dat'],
-    # },
+    package_data={  # Optional
+        'scalecodec.type_registry': ['*.json'],
+    },
 
     # Although 'package_data' is the preferred approach, in some case you may
     # need to place data files outside of your packages. See:
     # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files
     #
     # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    #data_files=[('scalecodec/type_registry', ['scalecodec/type_registry/*.json'])],  # Optional
+    # data_files=[('type_registry', ['scalecodec/type_registry/*.json'])],  # Optional
 
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
