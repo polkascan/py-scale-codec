5877c70126079220c0360319856318c363816729
Arjan Zijderveld <arjan@web3scan.eu>
2020-02-20 12:14:34 +0100

    Bumped copyright year

--- scalecodec/base.py
+++ scalecodec/base.py
@@ -1,6 +1,6 @@
 # Python SCALE Codec Library
 #
-# Copyright 2018-2019 openAware BV (NL).
+# Copyright 2018-2020 openAware BV (NL).
 # This file is part of Polkascan.
 #
 # Polkascan is free software: you can redistribute it and/or modify
