31bf46fbceafdb810c2b6d9f4f39b9675777d819
Arjan Zijderveld <arjan@web3scan.eu>
2020-07-11 21:56:50 +0200

    Updated requirements

--- setup.py
+++ setup.py
@@ -158,7 +158,7 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['more-itertools'],  # Optional
+    install_requires=['more-itertools', 'base58==1.0.3'],  # Optional
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
