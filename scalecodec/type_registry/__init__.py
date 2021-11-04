7a31298a328078306059437383e0d99b58024b8b
Arjan Zijderveld <5286904+arjanz@users.noreply.github.com>
2021-11-04 13:38:50 +0100

    Added Karura and Moonriver type registry

--- scalecodec/type_registry/__init__.py
+++ scalecodec/type_registry/__init__.py
@@ -21,7 +21,8 @@ from typing import Optional
 import requests
 
 SUPPORTED_TYPE_REGISTRY_PRESETS = ('canvas', 'default', 'development', 'kusama', 'polkadot', 'rococo', 'metadata_types',
-                                   'substrate-node-template', 'westend', 'statemint', 'statemine', 'test')
+                                   'substrate-node-template', 'westend', 'statemint', 'statemine', 'karura',
+                                   'moonriver', 'test')
 
 ONLINE_BASE_URL = 'https://raw.githubusercontent.com/polkascan/py-scale-codec/v1.0/scalecodec/type_registry/'
 
